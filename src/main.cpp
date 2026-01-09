ee5da4d63ccda88135472775dec9ec7877f4d239
Val'VR <valvrpro@gmail.com>
2026-01-09 22:42:27 +0100

    fix: bad event management

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@ int main()
     sf::RenderWindow window(sf::VideoMode({1920, 1080}), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-    sf::Sprite sprite;
-    sf::Texture texture;
-    sf::Texture texture2;
+    sf::Texture texture("assets/images/cards/card_back.png");
+    sf::Texture texture2("assets/images/cards/card_artichoke.png");
+    sf::Sprite sprite(texture);
 
-    if (!texture.loadFromFile("assets/images/cards/card_back.png")
-        || !texture2.loadFromFile("assets/images/cards/card_artichoke.png"))
-    {
-        return -1; // Error loading texture
-    }
+    // if (!texture.loadFromFile("assets/images/cards/card_back.png")
+    //     || !texture2.loadFromFile("assets/images/cards/card_artichoke.png"))
+    // {
+    //     return -1; // Error loading texture
+    // }
 
     while (window.isOpen())
     {
@@ -23,9 +23,9 @@ int main()
             if (event->is<sf::Event::Closed>())
                 window.close();
 
-            if (event->is<sf::Event::KeyPressed>(sf::Keyboard::Key::A))
+            if (event->is<sf::Event::KeyPressed>() && event->as<sf::Event::KeyPressed>().key.code == sf::Keyboard::Key::A)
                 sprite.setTexture(texture);
-            if (event->is<sf::Event::KeyPressed>(sf::Keyboard::Key::Z))
+            if (event->is<sf::Event::KeyPressed>() && event->as<sf::Event::KeyPressed>().key.code == sf::Keyboard::Key::Z)
                 sprite.setTexture(texture2);
                 // window.close();
         }
