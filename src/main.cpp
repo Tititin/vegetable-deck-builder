5604c2d78a3f5a36e6eaf9afe8fc960177f584c5
Val'VR <valvrpro@gmail.com>
2026-01-12 11:16:22 +0100

    fix: bad arguments for setScale method

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main()
     sf::Texture texture2("assets/images/cards/card_artichoke.png");
     sf::Sprite sprite(texture);
 
-    sprite.setScale(0.103f, 0.103f);
+    sprite.setScale({0.103f, 0.103f});
 
     // if (!texture.loadFromFile("assets/images/cards/card_back.png")
     //     || !texture2.loadFromFile("assets/images/cards/card_artichoke.png"))
