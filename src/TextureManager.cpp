9d5e4ff9f4aa5fc1bee0d67a745349bfc7387043
Val'VR <valvrpro@gmail.com>
2026-01-22 15:00:52 +0100

    feat: loaded textures for vegetable cards

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -4,6 +4,16 @@ TextureManager::TextureManager() { // Temporary constructor loading some texture
     _textures["card_back"] = sf::Texture("assets/images/cards/card_back.png");
     _textures["card_artichoke"] = sf::Texture("assets/images/cards/card_artichoke.png");
     _textures["card_onion"] = sf::Texture("assets/images/cards/card_onion.png");
+    _textures["card_corn"] = sf::Texture("assets/images/cards/card_corn.png");
+    _textures["card_potato"] = sf::Texture("assets/images/cards/card_potato.png");
+    _textures["card_eggplant"] = sf::Texture("assets/images/cards/card_eggplant.png");
+    _textures["card_peas"] = sf::Texture("assets/images/cards/card_peas.png");
+    _textures["card_carrot"] = sf::Texture("assets/images/cards/card_carrot.png");
+    // _textures["card_broccoli"] = sf::Texture("assets/images/cards/card_broccoli.png");
+    _textures["card_leek"] = sf::Texture("assets/images/cards/card_leek.png");
+    // _textures["card_rhubarb"] = sf::Texture("assets/images/cards/card_rhubarb.png");
+    _textures["card_bellpepper"] = sf::Texture("assets/images/cards/card_bellpepper.png");
+    _textures["card_beetroot"] = sf::Texture("assets/images/cards/card_beetroot.png");
     
     _textures["potager_slot"] = sf::Texture("assets/images/potager/potager_slot.png");
 }
