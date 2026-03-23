945251c17b2ac66ab2fcb5ed2f5bab34d5e52914
Val'VR <valvrpro@gmail.com>
2026-03-23 11:51:51 +0100

    feat: created files for PlayerHand class

---
+++ src/PlayerHand.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <map>
+#include "Clickable.hpp"
+#include "Card.hpp"
+#include "TextureManager.hpp"
+#include "InputManager.hpp"
+
+class PlayerHand : public Clickable {
+
+}
