94fb9aa96a94a5f58cf77beab9f9e837fa736322
Val'VR <valvrpro@gmail.com>
2026-03-31 15:36:50 +0200

    feat: First prototype of Garbage class

--- src/Garbage.cpp
+++ src/Garbage.cpp
@@ -1,1 +1,33 @@
 #include "Garbage.hpp"
+
+Garbage::Garbage(InputManager &inputManager, TextureManager &textureManager)
+: Clickable(textureManager.getTexture("potager_slot")),
+  _inputManager(&inputManager),
+  _textureManager(&textureManager)
+{
+}
+
+Garbage::~Garbage()
+{
+}
+
+void Garbage::addCard(Card *card)
+{
+    _cardsInGarbage.push_back(std::move(card));
+}
+
+void Garbage::setOnClick(ClickCallback callback)
+{
+}
+
+void Garbage::setOnClickRelease(ClickReleaseCallback callback)
+{
+}
+
+void Garbage::handleEvent(const sf::Event &event, const sf::RenderWindow &window)
+{
+}
+
+void Garbage::click()
+{
+}
--- src/Garbage.hpp
+++ src/Garbage.hpp
@@ -9,5 +9,27 @@
 #include "InputManager.hpp"
 
 class Garbage : public Clickable {
+private:
+    std::vector<Card*> _cardsInGarbage;
 
+    InputManager* _inputManager;
+    TextureManager* _textureManager;
+
+public:
+    Garbage(InputManager& inputManager, TextureManager& textureManager);
+    ~Garbage();
+
+    void addCard(Card* card);
+    const std::vector<Card*>& getCardsInGarbage() const { return _cardsInGarbage; }
+
+    // Callbacks
+    void setOnClick(ClickCallback callback);
+    void setOnClickRelease(ClickReleaseCallback callback);
+
+    // Event Handling
+    void handleEvent(const sf::Event& event, const sf::RenderWindow& window) override;
+    void click();
+
+    // Display
+    void draw(sf::RenderTarget& target) const { target.draw(_sprite); }
 };
