2680c93b7e26baa31f4997299dcdc1942ef56bb0
Val'VR <valvrpro@gmail.com>
2026-03-31 15:40:42 +0200

    feat: Create Game class files

---
+++ src/Game.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Card.hpp"
+#include "Deck.hpp"
+#include "Garbage.hpp"
+#include "PlayerHand.hpp"
+#include "Potager.hpp"
+#include "InputManager.hpp"
+#include "TextureManager.hpp"
