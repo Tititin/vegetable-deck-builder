aa0c099572fd5aafc2700e92ec29e77b2cc5455b
Val'VR <valvrpro@gmail.com>
2026-03-31 15:52:39 +0200

    feat: empty Game class

--- src/Game.hpp
+++ src/Game.hpp
@@ -8,3 +8,17 @@
 #include "Potager.hpp"
 #include "InputManager.hpp"
 #include "TextureManager.hpp"
+
+class Game {
+private:
+    // InputManager    _inputManager;
+    // TextureManager  _textureManager;
+    // Potager         _potager;
+    // Deck            _deck;
+    // Garbage         _garbage;
+    // PlayerHand      _playerHand;
+
+public:
+    Game();
+    ~Game();
+};
