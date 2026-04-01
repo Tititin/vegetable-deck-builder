e2eb54d3a41bb885121e99cf28e29110779b4050
Val'VR <valvrpro@gmail.com>
2026-04-01 10:56:00 +0200

    feat: CardManager prototype

--- src/CardManager.cpp
+++ src/CardManager.cpp
@@ -1,1 +1,31 @@
 #include "CardManager.hpp"
+
+CardManager::CardManager(TextureManager &textureManager)
+    : _textureManager(&textureManager)
+{
+}
+
+CardManager::~CardManager()
+{
+}
+
+void CardManager::init()
+{
+    _remainingCardsToCreate[Card::VegetableType::ARTICHOKE] = 10;
+    _remainingCardsToCreate[Card::VegetableType::ONION] = 6;
+    _remainingCardsToCreate[Card::VegetableType::CORN] = 6;
+    _remainingCardsToCreate[Card::VegetableType::POTATO] = 6;
+    _remainingCardsToCreate[Card::VegetableType::EGGPLANT] = 6;
+    _remainingCardsToCreate[Card::VegetableType::PEAS] = 6;
+    _remainingCardsToCreate[Card::VegetableType::CARROT] = 6;
+    _remainingCardsToCreate[Card::VegetableType::BROCCOLI] = 6;
+    _remainingCardsToCreate[Card::VegetableType::LEEK] = 6;
+    _remainingCardsToCreate[Card::VegetableType::RHUBARB] = 6;
+    _remainingCardsToCreate[Card::VegetableType::BELLPEPPER] = 6;
+    _remainingCardsToCreate[Card::VegetableType::BEETROOT] = 6;
+}
+
+Card *CardManager::createCard(const Card::VegetableType &type)
+{
+    return new Card(type, *_textureManager);
+}
--- src/CardManager.hpp
+++ src/CardManager.hpp
@@ -4,6 +4,19 @@
 #include <string>
 #include <map>
 #include "Clickable.hpp"
+#include "Card.hpp"
 
 class CardManager {
+protected:
+    std::map<Card::VegetableType, int> _remainingCardsToCreate; // Map to keep track of the number of each type of card in the deck
+
+    TextureManager* _textureManager;
+
+public:
+    CardManager(TextureManager& textureManager);
+    ~CardManager();
+
+    void init();
+
+    Card* createCard(const Card::VegetableType& type);
 };
