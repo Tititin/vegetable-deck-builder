693afbb0b490e28d012212027fc3fcb5ea943b19
Val'VR <valvrpro@gmail.com>
2026-04-13 16:11:30 +0200

    feat: BasicButton default params

--- src/ui/BasicButton.cpp
+++ src/ui/BasicButton.cpp
@@ -1,12 +1,15 @@
 #include "BasicButton.hpp"
 
-BasicButton::BasicButton(TextureManager &textureManager)
+BasicButton::BasicButton()
     : Clickable()
 {
 }
 
 void BasicButton::init()
 {
+    _buttonShape.setSize({BASIC_BUTTON_WIDTH, BASIC_BUTTON_HEIGHT});
+    _buttonShape.setFillColor(BASIC_BUTTON_COLOR);
+    _buttonShape.setPosition({1700.f, 20.f});
 }
 
 void BasicButton::handleEvent(const sf::Event &event, const sf::RenderWindow &window)
--- src/ui/BasicButton.hpp
+++ src/ui/BasicButton.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../lib/Params.hpp"
 #include "../Clickable.hpp"
 #include "../InputManager.hpp"
 #include "../TextureManager.hpp"
@@ -7,11 +8,16 @@
 class BasicButton : public Clickable {
     protected:
 
+    sf::RectangleShape _buttonShape;
+
     public:
-        BasicButton(TextureManager& textureManager);
+        BasicButton();
         ~BasicButton() = default;
 
-        void init() override;
-        void handleEvent(const sf::Event& event, const sf::RenderWindow& window) override;
-        void click() override;
+        virtual void init() override;
+        virtual void handleEvent(const sf::Event& event, const sf::RenderWindow& window) override;
+        virtual void click() override;
+        virtual bool contains(const sf::Vector2f& point) override { return _buttonShape.getGlobalBounds().contains(point); };
+
+        void draw(sf::RenderTarget& target) const { target.draw(_buttonShape); }
 };
